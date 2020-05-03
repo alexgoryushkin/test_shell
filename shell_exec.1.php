39b023f4233e3a79c636070df6a656d60ea83b54
Alexander Goryushkin <43167451+alexgoryushkin@users.noreply.github.com>
2020-05-03 19:58:50 +0500

    Update shell_exec.1.php

--- shell_exec.1.php
+++ shell_exec.1.php
@@ -1,12 +1,14 @@
 <html>
 	<head>
-		<meta charset="windows-1251">
+		<meta charset="utf8">
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<link rel="stylesheet" type="text/css" href="//bootswatch.com/4/flatly/bootstrap.min.css">
 		<title>Web Shell</title>
 	</head>
 	<body>
+		Hello!
+		<br>
 		<?php 
 			echo shell_exec('ping 8.8.8.8');
 		?>
